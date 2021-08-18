d112bce16c66bdd120d5c06314be51f2ee37f7d9
nikolai-git-house <nikolayhouse128@gmail.com>
2021-08-18 11:35:38 -0400

    simplify calculatorkey

--- src/components/Calculator/CalculatorKey.js
+++ src/components/Calculator/CalculatorKey.js
@@ -1,17 +1,13 @@
 import React from "react";
 
-class CalculatorKey extends React.Component {
-  render() {
-    const { onPress, className, ...props } = this.props;
-
-    return (
-      <button
-        className={`calculator-key ${className}`}
-        {...props}
-        onClick={onPress}
-      />
-    );
-  }
-}
+const CalculatorKey = ({ onPress, className, ...props }) => {
+  return (
+    <button
+      className={`calculator-key ${className}`}
+      {...props}
+      onClick={onPress}
+    />
+  );
+};
 
 export default CalculatorKey;
